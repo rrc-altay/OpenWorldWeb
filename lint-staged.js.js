b29bfe6475f576dda0a39726241559066070810c
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-05-17 13:13:16 +0500

    delete: bad integration  pre-commit

--- lint-staged.js.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "*.{ts,tsx}": [
-    "prettier --write src/",
-    "eslint . --fix",
-    "eslint . --ext .ts,.tsx",
-    "eslint --max-warnings=0",
-    "bash -c tsc",
-  ],
-};
