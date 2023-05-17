e12289017bf071d2055cc161a27c540dfc26b47e
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-05-17 13:18:36 +0500

    test: pre-commit

--- lint-staged.js
+++ lint-staged.js
@@ -1,10 +1,9 @@
 module.exports = {
-  "/src/**/*.{ts,tsx}": [
+  "./src/**/*.{ts,tsx}": [
     "prettier --write src/",
     "eslint . --fix",
     "eslint . --ext .ts,.tsx",
     "eslint --max-warnings=0",
-    "bash -c jest",
     "bash -c tsc",
   ],
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-
-        console.log(123);
+console.log("fix");
 
 export default function Document() {
   return (
