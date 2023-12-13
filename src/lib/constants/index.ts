447c51900273a1f5e62d5a3fc7c26fccb97f85f9
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-12-13 09:23:32 +0500

    update

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -2,7 +2,7 @@ export const BASE_URL = "https://rrc-altay.ru";
 export const API_URL = BASE_URL + "/api/v1/";
 export const PAGE_START = 1;
 export const PAGE_LIMIT = 7;
-export const TIMEOUT = 0;
+export const TIMEOUT = 60000;
 
 export const WORKER_URL =
   "https://unpkg.com/pdfjs-dist@3.7.107/build/pdf.worker.min.js";
