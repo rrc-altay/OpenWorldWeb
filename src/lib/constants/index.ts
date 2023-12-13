d9d4e3f47299c098363e7af7d97c6b9f8b5e3865
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-12-13 09:53:10 +0500

    fix

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://rrc-altay.ru";
+export const BASE_URL = "https://1openworld.ru";
 export const API_URL = BASE_URL + "/api/v1/";
 export const PAGE_START = 1;
 export const PAGE_LIMIT = 7;
