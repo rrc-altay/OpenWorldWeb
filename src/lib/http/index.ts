f440d3ae3cd64af029302b1440daa76ccd2d9e2d
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-12-13 11:06:56 +0500

    update

--- src/lib/http/index.ts
+++ src/lib/http/index.ts
@@ -1,9 +1,19 @@
 import axios from "axios";
 import { API_URL, TIMEOUT } from "@/lib/constants";
+import * as https from "https";
 
-const axiosProject = axios.create({
-  baseURL: API_URL,
-  timeout: TIMEOUT,
-});
+const instance = () => {
+  const config = axios.create({
+    baseURL: API_URL,
+    timeout: TIMEOUT,
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  });
+
+  return config;
+};
+
+const axiosProject = instance();
 
 export default axiosProject;
